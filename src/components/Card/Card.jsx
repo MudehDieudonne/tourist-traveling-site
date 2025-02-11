7fd2bc58eef3973d991f817e60333cd617b01283
https://github.com/MudehDieudonne <mukummudeh@gmail.com>
2025-02-11 19:29:30 +0100

    home page partially completer

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,23 @@
+import PropTypes from "prop-types"
 import './Card.css'
 
-function Card(props) {
+export function Card(props) {
     return (
         <div className="card">
             <img src={props.img} alt="title" />
+            <div className="card-details">
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+                <button>Learn More</button>
+            </div>
+        </div>
+    )
+}
+
+export function CardRnd(props) {
+    return (
+        <div className="cardrnd">
+            <img className="round" src={props.img} alt="title" />
             <div className="card-details">
                 <h3>{props.title}</h3>
                 <p>{props.description}</p>
@@ -11,5 +25,3 @@ function Card(props) {
         </div>
     )
 }
-
-export default Card
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "../components/Header/Header"
-import Card from "../components/Card/Card"
 import Footer from "../components/Footer/Footer"
+import { Card, CardRnd } from "../components/Card/Card"
 
 const Home = () => {
     return (
@@ -80,6 +80,43 @@ const Home = () => {
                 <h2>Select your Best Popular <br />Destination</h2>
             </div>
 
+            <div className="slider">
+                <CardRnd
+                    title={'Jirrafe'}
+                    img={'https://plus.unsplash.com/premium_photo-1724701624533-dea17ce5e61e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bmF0aXZlJTIwYWZyaWNhbiUyMGhvbWV8ZW58MHx8MHx8fDA%3D'}
+                    description={'Lorem Ispum beatch Hous '}
+                />
+
+                <CardRnd
+                    title={'Jirrafe'}
+                    img={'https://media.istockphoto.com/id/1149453484/photo/lanscape-with-mataya-village-of-sara-tribe-people-guera-chad.webp?a=1&b=1&s=612x612&w=0&k=20&c=HtCoSw-PBqR8CQnaKdbxDKT1Tffb2SmbJDM3KB4rbCI='}
+                    description={'Lorem Ispum beatch Hous '}
+                />
+
+                <CardRnd
+                    title={'Jirrafe'}
+                    img={'https://media.istockphoto.com/id/1962441295/photo/young-tuareg-with-camels-on-western-sahara-desert-in-africa.webp?a=1&b=1&s=612x612&w=0&k=20&c=0Tj5qDy7UoQ9uTynv7XobP2b-dUY5WLfA26DXlCgLD0='}
+                    description={'Lorem Ispum beatch Hous '}
+                />
+
+                <CardRnd
+                    title={'Jirrafe'}
+                    img={'https://images.unsplash.com/photo-1501183638710-841dd1904471?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG5hdGl2ZSUyMGFmcmljYW4lMjBob21lfGVufDB8fDB8fHww'}
+                    description={'Lorem Ispum beatch Hous '}
+                />
+
+                <CardRnd
+                    title={'Jirrafe'}
+                    img={'https://images.unsplash.com/photo-1510739628590-0c601822e0a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bmF0aXZlJTIwaG9tZXxlbnwwfHwwfHx8MA%3D%3D'}
+                    description={'Lorem Ispum beatch Hous '}
+                />
+            </div>
+
+            <div className="choose-package">
+                <h3>Popular Destinations</h3>
+                <h2>Select your Best Popular <br />Destination</h2>
+            </div>
+            <Footer />
         </main>
     )
 }
