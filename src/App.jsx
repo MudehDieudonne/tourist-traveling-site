96af8a43f674f61a43bc9526ae61fabe1378123a
https://github.com/MudehDieudonne <mukummudeh@gmail.com>
2025-02-23 19:35:15 +0100

    Contact and galerey page creation incomplete styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Raouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Pakage from "./pages/TourPakage"
+import Gallery from "./pages/Gallery"
+import Contact from "./pages/Contact"
 import './App.css'
 
 function App() {
@@ -12,6 +14,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/TourPakage" element={<Pakage />} />
+        <Route path="/Gallery" element={<Gallery />} />
+        <Route path="/Contact" element = {<Contact/>} />
+
       </Routes>
     </Raouter>
   )
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
+import Allhero from "../components/hero/hero"
+
+function Contact() {
+    return (
+        <>
+            <Header />
+            <Allhero />
+            <Footer />
+        </>
+    )
+}
+
+export default Contact
